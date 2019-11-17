5936b4c032ec60945e29cc3bd58b9beb816f1768
coaugusto <coaugusto@gmail.com>
2019-11-17 20:28:04 -0300

    confirmation appointment finished

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { withNavigationFocus } from 'react-navigation';
 import api from '~/services/api';
 
 import Background from '~/components/Background';
@@ -9,18 +10,24 @@ import Appointment from '~/components/Appointment';
 import { Container, Title, List } from './styles';
 
 
-export default function Dashboard() {
+function Dashboard({ isFocused }) {
 
   const [appointments, setAppointments] = useState([]);
 
-  useEffect(() => {
-    async function loadAppointments() {
-      const response = await api.get('appointments');
+  async function loadAppointments() {
+    const response = await api.get('appointments');
 
-      setAppointments(response.data);
+    setAppointments(response.data);
+  }
+  loadAppointments();
+
+
+  useEffect(() => {
+    if (isFocused) {
+      loadAppointments();
     }
-    loadAppointments();
-  }, []);
+  }, [isFocused]);
+
 
   async function handleCancel(id) {
     const response = await api.delete(`appointments/${id}`);
@@ -59,5 +66,8 @@ Dashboard.navigationOptions = {
   tabBarLabel: 'Agendamentos',
   tabBarIcon: ({ tintColor }) => (
     <Icon name="event" size={20} color={tintColor} />
-  )
-}
+  ),
+};
+
+export default withNavigationFocus(Dashboard);
+
